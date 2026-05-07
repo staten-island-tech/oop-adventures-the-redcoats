2b26d52b8ceb4559add6fc05a8b4afc589135e82
MochiKoffe <sophiem47@nycstudents.net>
2026-05-07 10:01:49 -0400

    h

--- euf.py
+++ euf.py
@@ -1,12 +1,20 @@
 import random
 
 class Soldier: 
+<<<<<<< Updated upstream
     def __init__(self, name, hunger=100, health=100, fun=100, ammo=30, living = True): 
+=======
+    def __init__(self, name, hunger=100, health=100, fun=100, ammo=30, shillings = 0, living = True): 
+>>>>>>> Stashed changes
         self.name = name
         self.hunger=hunger
         self.health=health
         self.fun=fun
         self.ammo=ammo
+<<<<<<< Updated upstream
+=======
+        self.shillings=shillings
+>>>>>>> Stashed changes
         self.living=living
     def byebye(self): 
         self.fun -= 10
@@ -20,11 +28,19 @@ class Soldier:
             print("You're surviving so far...")
         if self.ammo >0: 
             print(f"You have {self.ammo}")
+<<<<<<< Updated upstream
         if self.fun >=30: 
             print("You're getting bored!")
         if self.hunger >=30: 
             print("You're getting hungry!")
         if self.health>=50: 
+=======
+        if self.fun <=30: 
+            print("You're getting bored!")
+        if self.hunger<=30: 
+            print("You're getting hungry!")
+        if self.health<=50: 
+>>>>>>> Stashed changes
             print("You're low on health!")
         if self.ammo == 0: 
             print("You have no ammo!")
@@ -38,13 +54,59 @@ class Soldier:
         self.hunger -=5
         self.health -=20
         self.fun -=10
+<<<<<<< Updated upstream
+=======
+    def find(self): 
+        self.shillings +=5
+        self.fun +=10
+>>>>>>> Stashed changes
 
 Name = input("What do you call yourself, sir?")
 character = Soldier(Name)
 
 while character.living == True: 
     character.dec()
+<<<<<<< Updated upstream
     Userinput = input(f"You're told to stop the patriots at Lexington and Concord. What would you like to do?")
     Userinput =Userinput.lower()
     print("find supplies") 
     print("find patriots")
+=======
+    print("You're told to stop the patriots at Lexington and Concord by your commander.")
+    Userinput = input(f"What would you like to do?")
+    Userinput =Userinput.lower()
+    if "find supplies" in Userinput: 
+        print("You go searching...") 
+        character.byebye()
+        x = random.randint(1, 20)
+        if x <15: 
+            print("You've found nothing...") 
+        if x>=15: 
+            print("You've looted some supplies the Patriots stole!")
+            character.find()
+            character.fun +=20
+    if "find patriots" in Userinput: 
+        x = random.randint(1, 20)
+        print("You've found {x}!")
+        weaponchoice = input(f"Would you like to kill using a musket or sabre?")
+        if "musket" in weaponchoice: 
+            shoot = input(f"Press x to shoot!")
+            if "x" in shoot: 
+                x = random.randint(1, 20)
+                if x <=10: 
+                    print("Oh no! You missed! Now they've shot you!")
+                    character.shoot()
+                    character.damage()
+                    character.byebye()
+                elif x >10: 
+                    print("You've shot them!") #finish killing part tomorrow, add patriot class
+    if "loot and plunder" in Userinput:
+        character.byebye()
+        x = random.randint(1, 20)
+        if x <5: 
+            print("You've found some money!")
+            character.find()
+        elif x >=5: 
+            print("You've found nothing...")
+            character.byebye()
+>>>>>>> Stashed changes
