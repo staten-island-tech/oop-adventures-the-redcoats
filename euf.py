ef4b95ead3cbf83ec0045329a95f475eb139ac9f
MochiKoffe <sophiem47@nycstudents.net>
2026-05-08 10:17:14 -0400

    Update euf.py

--- euf.py
+++ euf.py
@@ -31,23 +31,23 @@ class Soldier:
         
     def byebye(self): 
         self.fun -= 10
-        self.hunger -= 20
+        self.hunger -= 5
     def dec(self): 
         if self.fun >30: 
             print("You're having fun!")
         if self.hunger>30: 
             print("You're full")
-        if self.health >30: 
+        if self.health >50: 
             print("You're surviving so far...")
         if self.ammo >0: 
             print(f"You have {self.ammo} ammo")
         if self.fun <=30: 
             print("You're getting bored!")
-            self.health -=10
-            self.hunger-=20
+            self.health -=1
+            self.hunger-=5
         if self.hunger <=30: 
             print("You're getting hungry!")
-            self.health -=30
+            self.health -=2
         if self.health<=50: 
             print(f"You have {self.health} health")
         if self.ammo == 0: 
@@ -78,7 +78,7 @@ level1 = input("Write play to begin the level.")
 if "play" in level1: 
     while character.living == True: 
         character.dec()
-        Userinput = input(f"What would you like to do?")
+        Userinput = input(f"What would you like to do? Loot and plunder, find supplies, or find patriots?")
         Userinput =Userinput.lower()
         if "find supplies" in Userinput: 
             print("You go searching...") 
@@ -106,6 +106,7 @@ if "play" in level1:
                     elif x >10: 
                         print("You've shot them!") 
                         kills += x
+                        character.shoot()
         if "loot and plunder" in Userinput:
             character.byebye()
             x = random.randint(1, 20)
@@ -115,7 +116,64 @@ if "play" in level1:
             elif x >=5: 
                 print("You've found nothing...")
                 character.byebye()
-        if kills == 20 and character.shillings == 30: 
+        if kills == 10 and character.shillings == 10: 
             level1 = "finished"
+    if character.living == False: 
+        character.death()
+        
+
+
 elif "finished" in level1: 
+    print("You've successfully completed your mission in Lexington and Concord!")
+    print("You're now ordered to take the land of Bunker's Hill")
+kills = 0
+character.hunger=100
+character.health = 100
+character.fun = 100
+character.ammo=30
+level2 = input("Write play to begin the level.")
+if "play" in level2: 
+    while character.living == True: 
+        character.dec()
+        Userinput = input(f"What would you like to do? Loot and plunder, find supplies, or find patriots?")
+        Userinput =Userinput.lower()
+        if "find supplies" in Userinput: 
+            print("You go searching...") 
+            character.byebye()
+            x = random.randint(1, 20)
+            if x <15: 
+                print("You've found nothing...") 
+            if x>=15: 
+                print("You've looted some supplies the Patriots stole!")
+                character.find()
+                character.fun +=20
+        if "find patriots" in Userinput: 
+            x = random.randint(1, 5)
+            print(f"You've found {x}!")
+            weaponchoice = input(f"Would you like to kill using a musket or sabre?")
+            if "musket" in weaponchoice: 
+                shoot = input(f"Press x to shoot!")
+                if "x" in shoot: 
+                    x = random.randint(1, 20)
+                    if x <=10: 
+                        print("Oh no! You missed! Now they've shot you!")
+                        character.shoot()
+                        character.damage()
+                        character.byebye()
+                    elif x >10: 
+                        print("You've shot them!") 
+                        kills += x
+                        character.shoot()
+        if "loot and plunder" in Userinput:
+            character.byebye()
+            x = random.randint(1, 20)
+            if x <5: 
+                print("You've found some money!")
+                character.find()
+            elif x >=5: 
+                print("You've found nothing...")
+                character.byebye()
+        if kills == 10 and character.shillings == 10: 
+            level2 = "finished"
+elif "finished" in level2: 
     print("You've successfully completed your mission in Lexington and Concord!")
